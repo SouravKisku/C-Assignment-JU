334f04334aa78bdcfc2d1c89cd50389d12f9930d
SouravKisku <souravkisku8@gmail.com>
2025-02-15 20:18:14 +0530

    updates

--- Assignment 5/code1.cpp
+++ Assignment 5/code1.cpp
@@ -1,45 +1,37 @@
 #include <iostream>
+using namespace std;
 
 class Circle {
-private:
-    double radius;
-    double area;
-
 protected:
-    double getRadius() const { // Protected function to allow derived classes to access radius
-        return radius;
-    }
+    double radius; // Protected member
 
 public:
-    Circle(double r) : radius(r), area(0.0) {}
-
-    void calculateArea() {
-        area = 3.14159 * radius * radius;
+    void setRadius(double r) { // Public method to set radius
+        radius = r;
     }
 
-    void displayArea() const {
-        std::cout << "Area of the circle: " << area << std::endl;
+    double getRadius() { // Public method to get radius
+        return radius;
     }
+};
 
-    // Function to set radius with validation
-    void setRadius(double r) {
-        if (r > 0) {
-            radius = r;
-        } else {
-            std::cout << "Invalid radius. Radius must be positive." << std::endl;
-        }
+class Area : public Circle {
+public:
+    double calculateArea() { // Public method to calculate area
+        return 3.14159 * radius * radius;
     }
 };
 
 int main() {
-    Circle myCircle(5.0);
-    myCircle.calculateArea();
-    myCircle.displayArea(); // Output: Area of the circle: 78.5397
-
-    // Example of using setRadius to modify the radius
-    myCircle.setRadius(7.0);
-    myCircle.calculateArea();
-    myCircle.displayArea(); // Output: Area of the circle: 153.937
-
+    Area circle;
+    double r;
+    
+    cout << "Enter the radius of the circle: ";
+    cin >> r;
+    
+    circle.setRadius(r); // Setting radius using public function
+    
+    cout << "The area of the circle is: " << circle.calculateArea() << endl;
+    
     return 0;
 }
